717452548e36ffe64d9b2f12ed44919266b555b5
IvanMladenov <ismladenov@gmail.com>
2015-09-30 12:32:48 +0300

    ThreeLargestNumbers for 90/100, time limit on one test

--- src/ThreeLargestNumbers.java
+++ src/ThreeLargestNumbers.java
@@ -1,5 +1,34 @@
-/**
- * Created by Ivan on 9/30/2015.
- */
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class ThreeLargestNumbers {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int numbersCount = Integer.parseInt(scanner.nextLine().trim());
+
+        BigDecimal[] numbers = new BigDecimal[numbersCount];
+
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = new BigDecimal(scanner.nextLine());
+
+        }
+        Arrays.sort(numbers, Collections.reverseOrder());
+        int endPoint;
+
+        if (numbersCount>=3){
+            endPoint=3;
+        }else {
+            endPoint=numbersCount;
+        }
+
+        for (int i = 0; i < endPoint; i++) {
+            if(i==numbersCount){
+                break;
+            }
+            System.out.println(numbers[i].toPlainString());
+        }
+    }
 }
